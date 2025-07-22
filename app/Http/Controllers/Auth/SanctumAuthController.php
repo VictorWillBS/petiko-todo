e82e7a0d3eb1b1c836e573b4e9e6eee482d7d170
Victow Barros <victorwbarros@gmail.com>
2025-07-22 03:28:11 -0300

    feat: create register to api

--- app/Http/Controllers/Auth/SanctumAuthController.php
+++ app/Http/Controllers/Auth/SanctumAuthController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class SanctumAuthController extends Controller
 {
+    public function __construct(protected User $user) {}
+
     public function login(Request $request)
     {
         $credentials = $request->validate([
@@ -31,5 +36,31 @@ class SanctumAuthController extends Controller
         $request->session()->regenerateToken();
 
         return response()->json(['message' => 'Logout feito com sucesso']);
+    }
+
+    public function register(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+
+        $user = $this->user->where('email', $credentials['email'])->first();
+
+        if ($user) {
+            return abort(409, 'Usuário já existe');
+        }
+
+        $user = $this->user->where('email', $credentials['email'])->firstOrCreate([
+            'name' => $credentials['name'],
+            'email' => $credentials['email'],
+            'password' => Hash::make($credentials['password']),
+        ]);
+
+        Auth::login($user); // faz login automático após registro
+        $request->session()->regenerate();
+
+        return response()->json(['message' => 'Usuário registrado com sucesso']);
     }
 }
